0933f91f1f89a8c7a2366ef36cbc24b57d4a04a4
trevor <trevor@matterport.com>
2016-12-06 06:17:46 -0800

    Day 6, Part 1

---
+++ adventofcode_2016/6.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+import collections
+
+if  len(sys.argv) != 2 or not os.path.isfile(sys.argv[1]) :
+	print "Invalid argument"
+	sys.exit(1)
+
+with open(sys.argv[1]) as input_file:
+	content = input_file.read().splitlines()
+
+columns=[collections.Counter() for i in range(8)]
+print columns
+
+for line in content:
+	for i,l in enumerate(line):
+		columns[i][l] += 1
+
+print columns
+
+password = [ i.most_common(1)[0][0] for i in columns ]
+
+print password
+
+print "The password is",''.join(password)
+
+sys.exit(0)
