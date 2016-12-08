425fd42673f3dcfa7f90d826cfd30780784c794b
trevor <trevor@matterport.com>
2016-12-07 17:27:56 -0800

    Mild tidying

--- adventofcode_2015/7.py
+++ adventofcode_2015/7.py
@@ -4,17 +4,14 @@ import sys
 import hashlib
 import json
 import os.path
-#import string
 
 def hashDict(dictionary):
 	return hashlib.md5(json.dumps(dictionary, sort_keys=True)).hexdigest()
 
 def getinput(wires,gate):
-	if gate.isdigit() :
-		return int(gate)
-	else:
-		return wires[gate]
+	return int(gate) if gate.isdigit() else wires[gate]
 
+# tick each operator until we reach steady state (hash doesn't change)
 def findSteadyState(wires,gates,wire):
 	hash=''
 	while hash != hashDict(wires) :
@@ -46,15 +43,15 @@ class ORGate(biGate):
 
 class LSHIFTGate(biGate):
 	def tick(self,wires):
-		wires[self.out] = getinput(wires,self.in_A) << self.in_B
+		wires[self.out] = getinput(wires,self.in_A) << getinput(wires,self.in_B)
 
 class RSHIFTGate(biGate):
 	def tick(self,wires):
-		wires[self.out] = getinput(wires,self.in_A) >> self.in_B
+		wires[self.out] = getinput(wires,self.in_A) >> getinput(wires,self.in_B)
 
 class NOTGate(monoGate):
 	def tick(self,wires):
-		wires[self.out] = 65536 - getinput(wires,self.in_A) - 1
+		wires[self.out] = 0xffff & ~ getinput(wires,self.in_A)
 
 class ASSIGNGate(monoGate):
 	def tick(self,wires):
@@ -71,27 +68,25 @@ for line in content:
 	if not line :
 		continue
 	parse=line.split()
-	if 'AND' in parse :
-		gates.append(ANDGate(parse[0],parse[2],parse[4],wires))
-	elif 'OR' in parse :
-		gates.append(ORGate(parse[0],parse[2],parse[4],wires))
-	elif 'LSHIFT' in parse :
-		gates.append(LSHIFTGate(parse[0],int(parse[2]),parse[4],wires))
-	elif 'RSHIFT' in parse :
-		gates.append(RSHIFTGate(parse[0],int(parse[2]),parse[4],wires))
-	elif 'NOT' in parse :
-		gates.append(NOTGate(parse[1],parse[3],wires))
-	else :
-		gates.append(ASSIGNGate(parse[0],parse[2],wires))
+	if 'AND' in parse:
+		gates.append(    ANDGate(parse[0],parse[2],parse[4],wires) )
+	elif 'OR' in parse:
+		gates.append(     ORGate(parse[0],parse[2],parse[4],wires) )
+	elif 'LSHIFT' in parse:
+		gates.append( LSHIFTGate(parse[0],parse[2],parse[4],wires) )
+	elif 'RSHIFT' in parse:
+		gates.append( RSHIFTGate(parse[0],parse[2],parse[4],wires) )
+	elif 'NOT' in parse:
+		gates.append(    NOTGate(parse[1],parse[3],wires) )
+	else:
+		gates.append( ASSIGNGate(parse[0],parse[2],wires) )
 
 findSteadyState(wires,gates,'a')
 
+# remove the gate that assigns a fixed value to b
 gates = [ gate for gate in gates if not gate.out == 'b' ]
-
+# and add a new one with a's value
 gates.append(ASSIGNGate(str(wires['a']),'b',wires))
-
-for key in wires:
-	wires[key]=0
 
 findSteadyState(wires,gates,'a')
 
