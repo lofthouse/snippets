c40341bfc561309190ccc2961d10d0883c730d9d
trevor <trevor@matterport.com>
2016-12-23 22:37:39 -0800

    Day 18, simplified, faster (since the actual rows don't matter)

--- adventofcode_2016/18.py
+++ adventofcode_2016/18.py
@@ -21,17 +21,13 @@ def rowFrom(row):
 if len(sys.argv) == 3:
 	num_rows=int(sys.argv[1])
 	with open(sys.argv[2]) as input_file:
-		rows=[ '.' + input_file.read().splitlines()[0] + '.' ]
-	safe_count += rows[0].count('.') - 2
+		row='.' + input_file.read().splitlines()[0] + '.'
+	safe_count += row.count('.') - 2
 else:
 	print "Usage: <rows> <row1_file.txt>"
 	sys.exit(0)
 
-while len(rows) < num_rows:
-	rows.append(rowFrom(rows[-1]))
-
-#for row in rows:
-#	print row[1:-1]
-#	safe_count += row.count('.') - 2
+for n in range(1,num_rows):
+	row=rowFrom(row)
 
 print "There are %d safe tiles" % safe_count
