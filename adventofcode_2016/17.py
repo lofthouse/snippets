9e0c555083c47beb7353c47611ad5a56143e4c88
trevor <trevor@matterport.com>
2016-12-23 20:38:49 -0800

    Day 17, with more elegance!

--- adventofcode_2016/17.py
+++ adventofcode_2016/17.py
@@ -10,42 +10,39 @@ else:
 	print "Need to know the passcode!"
 	sys.exit(0)
 
-def isLockedOrWall(x,y,passcode,movement):
+def isLockedOrWall(coord,passcode,movement):
 	opencodes='bcdef'
 	index='UDLR'.index(movement)
 
-	if x<0 or y<0 or x>3 or y>3:
+	if coord[0]<0 or coord[1]<0 or coord[0]>3 or coord[1]>3:
 		return True
 
 	return not md5(passcode).hexdigest()[index] in opencodes
 
-def tryPoint(moves,x,y,path,passcode,movement):
+def tryPoint(moves,coord,path,passcode,movement):
 	global visited,todo,end,paths,found_shortest
 
-	if not isLockedOrWall(x,y,passcode,movement):
-		if (x,y) == end:
+	if not isLockedOrWall(coord,passcode,movement):
+		if coord == end:
 			if not found_shortest:
 				found_shortest=True
 				print "The shortest path was %s moves: %s" % (moves,path)
 			else:
 				paths.add(moves)
 		else:
-			heappush(todo,(moves,(x,y),path,passcode+movement))
+			heappush(todo,(moves,coord,path,passcode+movement))
 todo=[]
 heapify(todo)
 paths=set()
 found_shortest=False
-start=(0,0)
-end=(3,3)
-path=''
-heappush(todo,(0,start,path,passcode))
+end=[3,3]
+vectors={'U':[0,-1],'D':[0,1],'L':[-1,0],'R':[1,0]}
 
+heappush(todo,(0,[0,0],'',passcode))
 while todo:
-	moves,(x,y),path,passcode=heappop(todo)
-	tryPoint(moves+1,x,y-1,path+'U',passcode,'U')
-	tryPoint(moves+1,x,y+1,path+'D',passcode,'D')
-	tryPoint(moves+1,x-1,y,path+'L',passcode,'L')
-	tryPoint(moves+1,x+1,y,path+'R',passcode,'R')
+	moves,coord,path,passcode=heappop(todo)
+	for vector in 'UDLR':
+		tryPoint(moves+1,map(sum,zip(coord,vectors[vector])),path+vector,passcode,vector)
 
 print "The longest path was: %s moves" % max(paths)
 sys.exit(0)
