c6979e56bfe12fc0dffaea5559e48cfa5e380628
trevor <trevor@matterport.com>
2017-02-12 20:07:26 -0800

    Day 16, Part 2

--- adventofcode_2015/16.py
+++ adventofcode_2015/16.py
@@ -15,6 +15,7 @@ def main():
 	with open( sys.argv[1] ) as input_file:
 		content = input_file.read().splitlines()
 
+	# load lab results
 	for line in content:
 		tmp = [ i.rstrip(':') for i in line.split() ]
 		lab_results[ tmp[0] ] = int( tmp[1] )
@@ -22,25 +23,35 @@ def main():
 	with open( sys.argv[2] ) as input_file:
 		content = input_file.read().splitlines()
 
+	# review each memory in turn
 	for line in content:
-		tmp = [ i.rstrip(':,') for i in line.split() ]
 		memory.clear()
-		failure = False
+		p1_failure = False
+		p2_failure = False
 
+		tmp = [ i.rstrip(':,') for i in line.split() ]
 		for i in range(2,7,2):
 			memory[ tmp[i] ] = int( tmp[i+1] )
 
-#		print lab_results
-#		print memory
-#		raw_input()
-
+		# attempt to find matches between the lab result and the memory
 		for key in lab_results:
-#			print "Testing key",key
-			if key in memory and lab_results[key] != memory[key]:
-				failure = True
+			if key in memory:
+				if memory[key] != lab_results[key]:
+					p1_failure = True
 
-		if not failure:
-			print "No Conflicts Found:",line
+				if key == 'cats' or key == 'trees':
+					if memory[key] <= lab_results[key]:
+						p2_failure = True
+				elif key == 'pomeranians' or key == 'goldfish':
+					if memory[key] >= lab_results[key]:
+						p2_failure = True
+				elif memory[key] != lab_results[key]:
+					p2_failure = True
+
+		if not p1_failure:
+			print "Part 1 No Conflicts Found:",line
+		if not p2_failure:
+			print "Part 2 No Conflicts Found:",line
 
 
 if __name__=="__main__":
