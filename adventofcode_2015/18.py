107cf0f2ea16f637498fc56f4f212ff5f4d25dd2
trevor <trevor@matterport.com>
2017-02-28 06:48:01 -0800

    Final Day 18

--- adventofcode_2015/18.py
+++ adventofcode_2015/18.py
@@ -2,7 +2,6 @@
 
 import sys
 import os.path
-import collections
 
 def usageAndExit():
 	print "18.py <grid_dim> <iterations> <input_file>"
@@ -18,26 +17,18 @@ def readGrid():
 	with open(sys.argv[3]) as input_file:
 		content = input_file.read().splitlines()
 
-	grid = [ [ 0 for i in range(dim) ] for i in range(dim) ]
-
-	j = 0
-	for line in content:
-		i = 0
-		for char in line:
-			grid[j][i] = 1 if char=='#' else 0
-			i = i + 1
-		j = j + 1
+	grid=[]
+	for j in range(dim):
+		grid.append( [ 1 if i=='#' else 0 for i in list(content[j]) ] )
 
 	return dim, iters, grid
 
-def step(dim, grid):
-	neighbor_counts = [ [ 0 for i in range(dim) ] for i in range(dim) ]
+def step(dim, grid, part2 = False):
 	result = [ [ 0 for i in range(dim) ] for i in range(dim) ]
 
 	for j in range(dim):
 		for i in range(dim):
 			neighbors = [ grid[y][x] for x in range(max(0,i-1),min(dim,i+2)) for y in range(max(0,j-1),min(dim,j+2)) ]
-			neighbor_counts[j][i] = sum(neighbors) - grid[j][i]
 
 			if grid[j][i] == 1:
 				if sum(neighbors) - 1 in (2,3):
@@ -45,19 +36,25 @@ def step(dim, grid):
 			else:
 				if sum(neighbors) == 3:
 					result[j][i] = 1
-
-	for j in 0,dim-1:
-		for i in 0,dim-1:
-			result[j][i] = 1
+	if part2:
+		for j in 0,dim-1:
+			for i in 0,dim-1:
+				result[j][i] = 1
 
 	return result
 
 def main():
-	dim, iters, grid = readGrid()
-	for _ in range(iters):
-		grid = step(dim,grid)
+	for mode in False,True:
+		dim, iters, grid = readGrid()
+		for _ in range(iters):
+			grid = step(dim,grid,mode)
 
-	print "There are %d lights on" % reduce(lambda x, y: x + sum(y), grid, 0)
+		count = 0
+		for row in grid:
+			count = count + sum(row)
+#			print ''.join(map(str,row))
+
+		print "There are %d lights on for %s" % (count, 'part 2' if mode==True else 'part 1')
 
 if __name__ == '__main__':
 	main()
