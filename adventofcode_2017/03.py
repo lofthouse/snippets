8638df07852a45f3c5291d71d55abab4161010ba
trevor <trevor@matterport.com>
2017-12-19 17:30:12 -0800

    Day 3, Part 2

--- adventofcode_2017/03.py
+++ adventofcode_2017/03.py
@@ -20,34 +20,44 @@ x = 0
 y = 0
 cursor = 1
 
+grid = {}
+grid[x,y] = 1
+part2_complete = False
+
 while cursor != data_square:
     # increment the cursor.  There are 5 possible "correct" movements
     # the movement must be determined first BEFORE we increment the cursor!
 
     # We have finished the circuit of a layer:  layer up!
     if x == layer and y == -layer:
-#        print "LAYER UP"
         layer = layer + 1
         x = x + 1
     # Right side:  move up
     elif x == layer and y != layer:
-#        print "MOVE UP"
         y = y + 1
     # Top side: move left
     elif y == layer and x != -layer:
-#        print "MOVE LEFT"
         x = x - 1
     # Left side: move down
     elif x == -layer and y != -layer:
-#        print "MOVE DOWN"
         y = y - 1
     # Bottom side: move right
     elif y == -layer and x != layer:
-#        print "MOVE RIGHT"
         x = x + 1
 
     cursor = cursor + 1
+    cell_value = 0
 
-#    print "The cursor (%d) is now at %d,%d" % (cursor,x,y)
+    # add up all neighboring cells (if present)
+    for xi in range(-1,2):
+        for yi in range(-1,2):
+            if (x+xi,y+yi) in grid:
+                cell_value = cell_value + grid[x+xi,y+yi]
+
+    grid[x,y] = cell_value
+
+    if not part2_complete and cell_value > data_square:
+        print "Part 2:  attempted to write %d at cell %d, which is greater than %d" % (cell_value,cursor,data_square)
+        part2_complete = True
 
 print "%d is at %d,%d for a Manhattan Distance of %d" % (data_square,x,y,abs(x)+abs(y))
