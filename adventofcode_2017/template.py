cd119e69e235c17dd838207f76ba5e9a30d6329e
trevor <trevor@matterport.com>
2017-12-20 10:35:30 -0800

    Day 06, Part 1

---
+++ adventofcode_2017/template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import os.path
+
+def getArgs():
+    if len(sys.argv) != 3 :
+        print "Invalid argument"
+        print "%s <input file> <part>" % sys.argv[0]
+        sys.exit(1)
+
+    if os.path.isfile(sys.argv[1]):
+        with open(sys.argv[1]) as input_file:
+            input = input_file.read().splitlines()
+    else:
+        print "%s is not a file" % sys.argv[1]
+        sys.exit(1)
+
+    part = int(sys.argv[2])
+
+    if not (part == 1 or part == 2):
+        print "%s is not a valid part" % sys.argv[2]
+        sys.exit(1)
+
+    return (input,part)
+
+# Begin actual code
+
+def main():
+    input,part = getArgs()
+
+if __name__=='__main__':
+    main()
