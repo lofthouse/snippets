8dd10575746669f62d554ece9bb6f2d1493a85a5
trevor <trevor@matterport.com>
2017-12-21 10:06:23 -0800

    Day 10, Part 2

--- adventofcode_2017/10.py
+++ adventofcode_2017/10.py
@@ -26,26 +26,31 @@ def getArgs():
         sys.exit(1)
 
     part = int(sys.argv[3])
-
-    if not (part == 1 or part == 2):
+    if part != 1 and part != 2:
         print "%s is not a valid part" % sys.argv[3]
         sys.exit(1)
 
-    return (map(int,input[0].split(',')), range(length), part)
+    if part == 1:
+        return (map(int,input[0].split(',')), range(length), part)
+    else:
+        return (map(ord,input[0]) + [17, 31, 73, 47, 23], range(length), part)
 
 # Begin actual code
 
-def main():
-    lengths,ring,part = getArgs()
+def densify(ring):
+    result = []
+    for group in range(16):
+        result.append( reduce(lambda x, y: x^y, ring[group*16:group*16+16]) )
 
-    position = 0
-    skip = 0
+    return result
+
+def hashRound(ring,position,skip,lengths):
     n = len(ring)
 
-    debug( ring )
+    debug( "Processing from %d with skip %d" % (position,skip) )
 
     for length in lengths:
-        debug( "Processing length %d" % length )
+#        debug( "Processing length %d" % length )
 
         start = position
         stop = position + length
@@ -58,10 +63,31 @@ def main():
         position = (position + length + skip) % n
         skip = skip + 1
 
-        debug( ring )
+#        debug( ring )
 
-    print "The answer is %d " % (ring[0] * ring[1])
+    return (ring,position,skip)
 
+
+def main():
+    lengths,ring,part = getArgs()
+
+    position = 0
+    skip = 0
+
+    debug( ring )
+
+    ring,position,skip = hashRound(ring,position,skip,lengths)
+
+    if part == 1:
+        print "The Part 1 answer is %d" % (ring[0] * ring[1])
+
+    if part == 2:
+        for i in range(63):
+            ring,position,skip = hashRound(ring,position,skip,lengths)
+
+        dense_ring = densify(ring)
+
+        print "The Part 2 answer is %s" % "".join(map(lambda x: format(x,'02x'),dense_ring))
 
 if __name__=='__main__':
     main()
