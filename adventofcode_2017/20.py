5482263cd84ee5f2c974ef2015d1677afb3efe92
trevor <trevor@matterport.com>
2017-12-26 19:17:50 -0800

    Day 20, Part 2

--- adventofcode_2017/20.py
+++ adventofcode_2017/20.py
@@ -21,9 +21,9 @@ def getArgs():
 
     part = int(sys.argv[2])
 
-#    if not (part == 1 or part == 2):
-#        print "%s is not a valid part" % sys.argv[2]
-#        sys.exit(1)
+    if not (part == 1 or part == 2):
+        print "%s is not a valid part" % sys.argv[2]
+        sys.exit(1)
 
     return (input,part)
 
@@ -35,36 +35,49 @@ def main():
     particles = []
 
     for line in input:
-        particles.append( dict(zip(['p','v','a'],map(lambda x: map(int,x),map(lambda x: x.split(','),map(lambda x: x.strip('<').split('>')[0],line.split('=')))[1:]))) )
+        particles.append( dict(zip(['p','v','a','active'],map(lambda x: map(int,x),map(lambda x: x.split(','),map(lambda x: x.strip('<').split('>')[0],line.split('=')))[1:]) + [True]) ) )
 
-    for tick in range(part):
+    for tick in range(1000):
         closest = [0,999999999]
         stable = True
 
         for i in range(len(particles)):
-            particles[i]['v'] = map( sum, zip(particles[i]['v'],particles[i]['a']) )
-            particles[i]['p'] = map( sum, zip(particles[i]['p'],particles[i]['v']) )
+            if particles[i]['active']:
+                particles[i]['v'] = map( sum, zip(particles[i]['v'],particles[i]['a']) )
+                particles[i]['p'] = map( sum, zip(particles[i]['p'],particles[i]['v']) )
 
-            dist = sum(map(abs,particles[i]['p']))
-            if dist < closest[1]:
-                closest = [i,dist]
+                dist = sum(map(abs,particles[i]['p']))
+                if dist < closest[1]:
+                    closest = [i,dist]
 
-#            pv = [ x*y for x,y in zip(particles[i]['p'],particles[i]['v']) ]
-#            va = [ x*y for x,y in zip(particles[i]['v'],particles[i]['a']) ]
+                if stable and (
+                # The sign of position and velocity need to match
+                    not all( p*v >= 0 for p,v in zip(particles[i]['p'],particles[i]['v']) ) or
+                # The sign of velocity and acceleration need to match and
+                # zero velocity is only allowed if zero acceleration
+                    not all( a == 0 or v*a > 0 for v,a in zip(particles[i]['v'],particles[i]['a']) )
+                    ):
+                    stable = False
 
-            if stable and (
-            # The sign of position and velocity need to match
-                not all( p*v >= 0 for p,v in zip(particles[i]['p'],particles[i]['v']) ) or
-            # The sign of velocity and acceleration need to match and
-            # zero velocity is only allowed if zero acceleration
-                not all( a == 0 or v*a > 0 for v,a in zip(particles[i]['v'],particles[i]['a']) )
-                ):
-                stable = False
+        if part == 2:
+            to_destroy = []
+
+            for p1,p1_data in enumerate(particles):
+                for p2,p2_data in enumerate(particles):
+                    if p1 != p2 and p1_data['active'] and p2_data['active'] and p1_data['p'] == p2_data['p']:
+                        to_destroy = to_destroy + [p1,p2]
+
+            for p in to_destroy:
+                particles[p]['active'] = False
 
         if stable:
             break
 
     print "%d is closest at %d and the simulation is stable at tick %d" % tuple(closest + [tick])
+
+    if part == 2:
+        survivors = len( [x for x in particles if x['active'] ] )
+        print "There are %d surviving particles" % survivors
 
 if __name__=='__main__':
     main()
