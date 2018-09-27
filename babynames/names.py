51190425eac1fb17a132465edc81b61d4d9f1607
trevor <trevor@matterport.com>
2018-09-27 08:50:52 -0700

    Slightly less hacky

--- babynames/names.py
+++ babynames/names.py
@@ -7,6 +7,21 @@ def debug(*args):
     if 'DEBUG' in os.environ:
         print(" ".join(map(str,args)) )
 
+def stuff(file,list,stats):
+    with open(file) as input_file:
+        input = input_file.read().splitlines()
+        for line in input:
+            line = line.replace(',', '')
+            _,m_name,m_stat,f_name,f_stat = line.split()
+
+            stats[ m_name ] = stats[ m_name ] + int( m_stat )
+            stats[ f_name ] = stats[ f_name ] + int( f_stat )
+
+            list[0].add( m_name )
+            list[1].add( f_name )
+
+    return (list, stats)
+
 def getArgs():
     if (len(sys.argv) - 1 ) % 2 != 0 :
         print("Invalid argument")
@@ -15,40 +30,36 @@ def getArgs():
 
     sys.argv.pop(0)
 
-    names_m_good = set()
-    names_f_good = set()
-    names_m_bad = set()
-    names_f_bad = set()
+    names_good = [ set(), set() ]
+    names_bad = [ set(), set() ]
     stats = defaultdict(int)
 
+    includes = []
+    excludes = []
+
     while len(sys.argv) > 1:
-        if os.path.isfile(sys.argv[1]):
-            with open(sys.argv[1]) as input_file:
-                input = input_file.read().splitlines()
-                for line in input:
-                    line = line.replace(',', '')
-                    _,m_name,m_stat,f_name,f_stat = line.split()
+        file = sys.argv.pop()
+        mode = sys.argv.pop()
 
-                    stats[ m_name ] = stats[ m_name ] + int( m_stat )
-                    stats[ f_name ] = stats[ f_name ] + int( f_stat )
-
-                    if sys.argv[0] == "-i":
-                        names_m_good.add( m_name )
-                        names_f_good.add( f_name )
-                    elif sys.argv[0] == "-e":
-                        names_m_bad.add( m_name )
-                        names_f_bad.add( f_name )
-                    else:
-                        print("%s is not a valid file command" % sys.argv[0])
-                        sys.exit(1)
+        if os.path.isfile( file ):
+            if mode == "-i":
+                includes.append( file )
+            elif mode == "-e":
+                excludes.append( file )
+            else:
+                print("%s is not a valid file command" % mode)
+                sys.exit(1)
         else:
-            print("%s is not a file" % sys.argv[1])
+            print("%s is not a file" % file)
             sys.exit(1)
 
-        sys.argv.pop(0)
-        sys.argv.pop(0)
+    for file in includes:
+        names_good,stats = stuff(file,names_good,stats)
 
-    return (names_m_good.difference(names_m_bad), names_f_good.difference(names_f_bad), stats)
+    for file in excludes:
+        names_bad,stats = stuff(file,names_bad,stats)
+
+    return (names_good[0].difference(names_bad[0]), names_good[1].difference(names_bad[1]), stats)
 
 # Begin actual code
 
@@ -56,11 +67,14 @@ def main():
     m,f,stats = getArgs()
 
     print("Good Male Names:")
-    print(sorted(m))
+    for name in sorted(m):
+        print(name)
+
+    print("\n=====\n")
 
     print("Good Female Names:")
-    print(sorted(f))
-
+    for name in sorted(f):
+        print(name)
 
 
 if __name__=='__main__':
