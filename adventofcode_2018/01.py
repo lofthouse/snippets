c070a8da4f00b663aa8c03fe97275217e238ecc9
trevor <trevor@matterport.com>
2018-11-30 20:29:26 -0800

    Let's go 2018 AOC!

---
+++ adventofcode_2018/01.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import os
+import sys
+
+def readfile():
+    if len(sys.argv) != 2 or not os.path.isfile( sys.argv[1] ):
+        print( f"Usage:  {sys.argv[0]} <input_file>" )
+        sys.exit(1)
+
+def main():
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
