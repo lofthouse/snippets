59782b6a903a28a239be52753197f50aa856926c
trevor <trevor@matterport.com>
2018-12-01 22:09:34 -0800

    Now _that's_ an elegant solution

--- adventofcode_2018/02.py
+++ adventofcode_2018/02.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import os
 import sys
-from collections import defaultdict
+from collections import Counter
 
 def readfile():
     if len(sys.argv) != 2 or not os.path.isfile( sys.argv[1] ):
@@ -11,38 +11,22 @@ def readfile():
     with open( sys.argv[1] ) as in_file:
         return in_file.read().splitlines()
 
-
 def main():
     boxes = readfile()
 
     box_contents = []
+    cs = 1
     for box in boxes:
-        foo = defaultdict(int)
-        for l in box:
-            foo[l] += 1
-        box_contents.append( foo )
-    
-    counts = defaultdict( int )
-    for box in box_contents:
-        for count in [2,3]:
-            for bin in box:
-                if box[bin] == count:
-                    counts[count] += 1
-                    break
+        box_contents.append( Counter(box) )
+    for count in [2,3]:
+        cs *= sum( [1 for box in box_contents if count in box.values() ] )
+    print( f"The checksum is {cs}" )
 
-    print( f"The checksum is {counts[2] * counts[3]}" )
-
-    for i,boxa in enumerate(boxes):
-        for boxb in boxes[i+1:]:
-            miss = 0
-            answer = ""
-            for j in range( len( boxa )):
-                if boxa[j] != boxb[j]:
-                    miss += 1
-                else:
-                    answer += boxa[j]
-            if miss == 1:
-                print( f"The common box ID letters are {answer}" )
+    for i,box_a in enumerate(boxes):
+        for box_b in boxes[i+1:]:
+            common_letters = [ al for al,bl in zip(box_a,box_b) if al == bl ]
+            if len(common_letters) == len(box_b) - 1:
+                print( f"The common box ID letters are {''.join(common_letters)}" )
                 sys.exit(0)
 
 if __name__ == "__main__":
