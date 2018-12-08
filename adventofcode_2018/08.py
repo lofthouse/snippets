9f66f56735a282c622a05bf01867871163026856
trevor <trevor@matterport.com>
2018-12-08 14:29:02 -0800

    Day 8 housekeeping

--- adventofcode_2018/08.py
+++ adventofcode_2018/08.py
@@ -10,30 +10,33 @@ def readfile():
     with open( sys.argv[1] ) as in_file:
         return in_file.read().splitlines()[0]
 
-
+# returns the value,total length, and summed metadata for the node and all embedded children
 def mdsum( data ):
     children = data[0]
     md = data[1]
 
+    # offset will record the total length of the node and all embeds:  it's at least 2 and we'll add
+    # to it as we resolve any children
     offset = 2
-    mds = 0
+    md_sums = 0
     value = 0
-    cvs = []
+    # child_values will store the values from each child in order so we can compute our own value later
+    child_values = []
     for i in range( children ):
         v,l,s = mdsum( data[offset:-md])
-        mds += s
+        child_values.append(v)
         offset += l
-        cvs.append(v)
+        md_sums += s
 
-    if children == 0:
-        value = sum( data[offset:offset+md] )
-    else:
-        for m in range(md):
-            child = data[offset+m]
-            if child-1 < len(cvs):
-                value += cvs[child-1]
+    if children == 0: # value is simply the sum or our metadata:  they start at the offset we've found
+        value = sum( data[ offset:offset + md ] )
+    else: # value is the sum of each named child's value, if it exists
+        for child in data[ offset:offset + md ]:
+            # children are 1-indexed;  boooo!
+            if child - 1 < len( child_values ):
+                value += child_values[ child - 1 ]
 
-    return ( value, offset + md, mds + sum( data[offset:offset+md]) )
+    return ( value, offset + md, md_sums + sum( data[ offset:offset + md ] ) )
 
 def main():
     data = [ int(i) for i in readfile().split() ]
