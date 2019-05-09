7c5e6845fa6b4a1db23cda0ac3d621aed2c9b22e
trevor <trevor@matterport.com>
2019-05-08 23:00:41 -0700

    Fixed bug!  Added formatting and comments

--- clockhands/hands.py
+++ clockhands/hands.py
@@ -2,24 +2,34 @@
 
 # h = hour
 # m = minute
-# hp = hour hand position (0-1, 12ths of a revolution)
-# mp = minute hand position (0-1, 12ths of a revolution)
+# hp = hour hand position (0-1, 1 = full revolution)
+# mp = minute hand position (0-1, 1 = full revolution)
 # error = maximum allowable error before we spit out a match
 
 h = m = hp = mp = 0
-error = 0.000000000001
+error = 0.00000000001
 
 # start at 00:00 and go until 12:00
 while h < 12:
     # if the hands line up, pretty-print the time, then move the hour hand forward 1 hour
     if abs(hp-mp) < error:
         h = int(hp * 12)
-        m = int(mp * 60)
-        s = 60 * (mp * 60 - m)
-        print( f"Match at {h}:{m}:{s}" )
+        m = int(round(mp * 60,0))
+        s = int(round(60 * (mp * 60 - m),0))
+
+        # pesky negative seconds and whole minutes...
+        if s < 0:
+            s += 60
+            m -= 1
+        if m == 60:
+            h += 1
+            m = 0
+
+        print( f"Match at {h}:{m:02d}:{s:02d}" )
         h += 1
         hp = h / 12
     # if they don't, advance the minute hand to where the hour hand was, then advance the hour hand
     else:
         mp = hp
-        hp = (h/12 + mp/60)
+        # this is the tricky bit:  mp is 0-1, and that's the portion of 1/12 a rotation that gets added to hp
+        hp = (h/12 + mp/12)
