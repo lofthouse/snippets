4901528c62777deb8045dba70dbd75891b112fea
trevor <trevor@matterport.com>
2019-06-29 10:21:30 -0700

    unified warrior class

--- adventofcode_2018/15.py
+++ adventofcode_2018/15.py
@@ -3,34 +3,37 @@ import os
 import sys
 from pprint import pprint
 
+# To facilitate reading order, all coordinates are are flipped when storing
+# x,y -> foo[ (y,x) ]
+
 opens = set()
 walls = set()
-goblins = dict()
-elves = dict()
+warriors = dict()
+x = 0
+y = 0
 
 class warrior:
     ap = 3
     hp = 300
 
-def printgrid(x,y):
+    def __init__(self,t):
+        self.type=t
+
+def printgrid():
     for j in range(y):
         for i in range(x):
             if (i,j) in walls:
                 print('#',end='')
             if (i,j) in opens:
                 print('.',end='')
-            if (i,j) in goblins:
-                print('G',end='')
-            if (i,j) in elves:
-                print('E',end='')
+            if (i,j) in warriors:
+                print(warriors[ (i,j) ].type,end='')
 
         print('  ', end='')
 
         for i in range(x):
-            if (i,j) in goblins:
-                print('G(',goblins[ (i,j) ].hp,'), ',end='')
-            if (i,j) in elves:
-                print('E(',elves[ (i,j) ].hp,'), ',end='')
+            if (i,j) in warriors:
+                print(warriors[ (i,j) ].type,'(',warriors[ (i,j) ].hp,'), ',end='')
 
         print('')
 
@@ -45,24 +48,26 @@ def readfile():
 
 
 def main():
-    x = y = 0
-
+    global x
+    global y
     lines = readfile()
     for j,line in enumerate(lines):
         for i,element in enumerate(line):
             if element == ".":
                 opens.add( (i,j) )
-            if element == "#":
+            elif element == "#":
                 walls.add( (i,j) )
-            if element == "G":
-                goblins[ (i,j) ] = warrior()
-            if element == "E":
-                elves[ (i,j) ] = warrior()
+            else:
+                warriors[ (i,j) ] = warrior(element)
 
     x = i + 1
     y = j + 1
 
-    printgrid(x,y)
+    printgrid()
+
+    pprint( warriors )
+    pprint( sorted(warriors) )
+    pprint( warriors )
 
 
 if __name__ == "__main__":
