7d5e5d0a20f69bc20a4ad6fa359f95d502f3b662
trevor <trevor@matterport.com>
2019-12-04 08:19:07 -0800

    Day 4, now with more elegance

--- adventofcode_2019/04.py
+++ adventofcode_2019/04.py
@@ -2,11 +2,12 @@
 import os
 import sys
 
-def test(c,Part2):
+def test(c):
     e = [ int(i) for i in str(c) ]
     max = len(e)
 
-    passed = False
+    # Part 1, Part 2
+    passed = [ False, False ]
     # 112222 is good, but fail happens after pass, pass needs to override
     # we we'll keep track of good and bad digits
     bad_digit = ''
@@ -14,37 +15,34 @@ def test(c,Part2):
 
     for j in range( max - 1 ):
         if e[j] == e[j+1]:
-            if not Part2:
-                passed = True
-            else:
-                if j < max-2 and e[j+1] == e[j+2]:
-                    passed = False
-                    bad_digit = e[j]
-                    if good_digit == bad_digit:
-                        good_digit = ''
-                elif e[j] != bad_digit:
-                    passed = True
-                    good_digit = e[j]
+            # Part 1 only needs a single pair:  PASS
+            passed[0] = True
+            if j < max-2 and e[j+1] == e[j+2]:
+                passed[1] = False
+                bad_digit = e[j]
+                if good_digit == bad_digit:
+                    good_digit = ''
+            elif e[j] != bad_digit:
+                passed[1] = True
+                good_digit = e[j]
         if e[j] > e[j+1]:
-            return False
+            return [ False, False ]
 
     if good_digit != '':
-        return True
+        # If we got here, both parts Passed
+        return [ True, True ]
 
     return passed
 
 def main():
-    valid1 = 0
-    valid2 = 0
+    valid = [ 0, 0 ]
 
     for i in range( 273025, 767253+1 ):
-        if test(i,False):
-            valid1 += 1
-        if test(i,True):
-            valid2 += 1
+        # This actually doesn't take any more time:  python does the right thing and doesn't recompute test(i)
+        valid = [ v+1 if r else v for v,r in zip( valid, test(i) ) ]
 
-    print( "There are", valid1, "part 1 passwords" )
-    print( "There are", valid2, "part 2 passwords" )
+    print( "There are", valid[0], "part 1 passwords" )
+    print( "There are", valid[1], "part 2 passwords" )
 
 
 if __name__ == "__main__":
