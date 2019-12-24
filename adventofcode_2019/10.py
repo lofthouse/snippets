5e961f085f9cbda5af6e0d2752fb496b41643841
trevor <trevor@matterport.com>
2019-12-24 11:45:13 -0800

    Part 2!  Now to simplify...

--- adventofcode_2019/10.py
+++ adventofcode_2019/10.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python3
 import argparse
 from collections import defaultdict
+from queue import PriorityQueue
+from math import atan2, pi
+from pprint import pprint
 
-parser = argparse.ArgumentParser(description='Advent of Code 2019 Day N')
+parser = argparse.ArgumentParser(description='Advent of Code 2019 Day 10')
 parser.add_argument('input_file', type=argparse.FileType('r'))
 parser.add_argument("-v", "--verbose", help="Include (useful) debug messages",action="store_true")
-#parser.add_argument("x", type=int, help="the number x")
+parser.add_argument("n", type=int, help="the nth asteroid to vaporize")
 args = parser.parse_args()
 
 # global factors lookup table used to find integer basis for all vectors
@@ -57,6 +60,11 @@ def basis( origin, destination ):
     # FWIW, it makes no time difference in python
     return tuple( i // mcd for i in basis )
 
+# return a value that increases from a minimum at pi/2 to a max at pi/2 - infinitesimal
+# remember that the y axis is flipped in elf-land! (positive DOWN)
+def heading( origin, dest ):
+    return (pi/2 + atan2( dest[1]-origin[1],dest[0]-origin[0] )) % (2*pi)
+
 def part_1( asteroids ):
     vis_counts = defaultdict( list )
 
@@ -65,32 +73,49 @@ def part_1( asteroids ):
 
         for other in asteroids:
             if other != home:
-                if args.verbose:
-                    print( "Adding basis for", other, "from", home )
-
+                # just blindly store basis of all other vectors
+                # duplicates (e.g. invisible) disappear into the same basis
                 visible.add( basis( home, other ) )
-
-                if args.verbose:
-                    print( visible )
-
-        if args.verbose:
-            print( "I can see", len( visible ), "asteroids from", home )
 
         vis_counts[ len( visible ) ].append( home )
 
-        if args.verbose:
-            print( vis_counts )
-
     best_count = max( vis_counts )
 
-    print( "The best observatory is at", vis_counts[ best_count ], "where I can see", best_count )
+    print( "The best observatory is at", vis_counts[ best_count ][0], "where I can see", best_count )
+    return vis_counts[ best_count ][0]
 
+# for part 2, we'll re-do the math for the winner, but this time
+# we'll store each point in a list in a priority queue ordered by the angle
+# in radians clockwise from pi/2
+# extraction then is just pop-ing one-at-a-time off each list going round
+# the priority queue
+def part_2( asteroids, winner ):
+    starchart = defaultdict( PriorityQueue )
+
+    for other in asteroids:
+        if other != winner:
+            starchart[ heading( winner, other ) ].put( ( sum( abs(b-a) for a,b in zip( winner, other ) ), other ) )
+
+    if args.verbose:
+        for r in sorted( starchart.keys() ):
+            print( r,": ", starchart[ r ].queue )
+
+    i = 0
+    while True:
+        for r in sorted( starchart.keys() ):
+            last = starchart[ r ].get()[1]
+            if starchart[ r ].empty():
+                del starchart[ r ]
+            i += 1
+            if not i < args.n:
+                print( "The", args.n, "asteroid to be obliterated was at", last, ". Submit", 100*last[0]+last[1] )
+                return
 
 def main():
     asteroids = readfile()
 
-    part_1( asteroids )
-
+    winner = part_1( asteroids )
+    part_2( asteroids, winner )
 
 if __name__ == "__main__":
     main()
