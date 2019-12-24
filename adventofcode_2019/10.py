bc55b2d65421b83d540f3d840957664e0a2c708f
trevor <trevor@matterport.com>
2019-12-24 11:54:08 -0800

    Now with faster

--- adventofcode_2019/10.py
+++ adventofcode_2019/10.py
@@ -3,18 +3,12 @@ import argparse
 from collections import defaultdict
 from queue import PriorityQueue
 from math import atan2, pi
-from pprint import pprint
 
 parser = argparse.ArgumentParser(description='Advent of Code 2019 Day 10')
 parser.add_argument('input_file', type=argparse.FileType('r'))
 parser.add_argument("-v", "--verbose", help="Include (useful) debug messages",action="store_true")
 parser.add_argument("n", type=int, help="the nth asteroid to vaporize")
 args = parser.parse_args()
-
-# global factors lookup table used to find integer basis for all vectors
-# every integer in the range 1-largest_dimension will have a factor list like
-# 24: [ 1,2,3,4,6,8,12 ]
-factors = {}
 
 def readfile():
     with args.input_file as input_file:
@@ -28,37 +22,7 @@ def readfile():
             if state == "#":
                 asteroids.add( (x,y) )
 
-    # build the factor table for each integer
-    # 1 and (self) are trivial cases and added manually to cut comp time by 1/2!
-    for i in range( 1, max(x,y) + 1 ):
-        factors[i] = [ 1 ]
-        for f in range( 2, i // 2 + 1 ):
-            if i % f == 0:
-                factors[i].append( f )
-        factors[i].append( i )
-
     return asteroids
-
-# return the integer basis for the vector from origin to destination
-# it's the (destination-origin)/mcd where mcd is the maximum common factor
-# shared by the absolute value of both coordinates of origin and destination
-def basis( origin, destination ):
-    global factors
-
-    basis = [ b-a for a,b in zip( origin, destination ) ]
-
-    # if a coordinate is 0, then scale the other axis by its mcd instead
-    if all( i != 0 for i in basis ):
-        if args.verbose:
-            print( "Finding basis for", basis )
-
-        mcd = max( set( factors[ abs( basis[0] ) ]).intersection( set( factors[ abs( basis[1] ) ] ) ) )
-    else:
-        mcd = max( factors[ max( ( abs(j) for j in basis ) ) ] )
-
-    # integer division!  no FP ops required, since everything is integer by definition!
-    # FWIW, it makes no time difference in python
-    return tuple( i // mcd for i in basis )
 
 # return a value that increases from a minimum at pi/2 to a max at pi/2 - infinitesimal
 # remember that the y axis is flipped in elf-land! (positive DOWN)
@@ -73,9 +37,9 @@ def part_1( asteroids ):
 
         for other in asteroids:
             if other != home:
-                # just blindly store basis of all other vectors
+                # just blindly store heading of all other vectors
                 # duplicates (e.g. invisible) disappear into the same basis
-                visible.add( basis( home, other ) )
+                visible.add( heading( home, other ) )
 
         vis_counts[ len( visible ) ].append( home )
 
@@ -96,12 +60,9 @@ def part_2( asteroids, winner ):
         if other != winner:
             starchart[ heading( winner, other ) ].put( ( sum( abs(b-a) for a,b in zip( winner, other ) ), other ) )
 
-    if args.verbose:
-        for r in sorted( starchart.keys() ):
-            print( r,": ", starchart[ r ].queue )
-
     i = 0
     while True:
+        # we'll be deleting empty headings, so iterate on the current list for this round
         for r in sorted( starchart.keys() ):
             last = starchart[ r ].get()[1]
             if starchart[ r ].empty():
